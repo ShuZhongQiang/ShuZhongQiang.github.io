a34dcd1b24dc89d471d806b1c19ac25220195f3b
szq <1372606388@qq.com>
2021-12-28 10:12:34 +0800

    Site updated: 2021-12-28 10:12:31

--- js/chocolate.js
+++ js/chocolate.js
@@ -76,7 +76,7 @@ btf.isJqueryLoad(function() {
             radius: 10,
             density: .2,
             color: 'rgba(255,255,255,.7)',
-            clearOffset: 0
+            clearOffset: 0.99
         });
     }! function(p) {
         p.fn.circleMagic = function(t) {
@@ -94,7 +94,6 @@ btf.isJqueryLoad(function() {
                     for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
                 requestAnimationFrame(h)
             }
-
             function f() {
                 var t = this;
 
@@ -103,16 +102,21 @@ btf.isJqueryLoad(function() {
                     t.pos.y = 0,
                     t.alpha = .1 + Math.random() * d.clearOffset,
                     t.scale = .1 + .3 * Math.random(),
-                    t.speed = Math.random(),
+                    t.speed = Math.random() + .3,
                     "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
                     t.pos = {},
                     e(),
                     this.draw = function() {
-                        t.alpha <= 0 && e(),
-                        t.pos.y += t.speed, t.alpha -= 5e-4,
+                        t.alpha <= 0 && e(), // 判断 alpha 小于 0 就调用 e()
+                        t.pos.y += t.speed,
+                        t.pos.x += Math.random() <= 0.5 ? -Math.random() : Math.random(),
+                        // t.alpha -= 5e-4, // alpha 每次 draw 减少 0.005
                         r.beginPath(),
                         r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1),
                         r.fillStyle = t.color, r.fill(), r.closePath()
+                        if (t.pos.y >= a) {
+                            t.alpha = 0
+                        }
                 }
             }! function() {
                 o = l.offsetWidth, a = l.offsetHeight,
